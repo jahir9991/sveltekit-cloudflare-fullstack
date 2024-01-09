ca5ddd85c3c2219ecfdc47d134a4bef00033c031
jahir alam <work.jahiralam@gmail.com>
2024-01-09 22:49:38 +0600

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	ssr: {
-		external: ['reflect-metadata','tsyringe']
-	}
+	plugins: [sveltekit()]
+	
 });
