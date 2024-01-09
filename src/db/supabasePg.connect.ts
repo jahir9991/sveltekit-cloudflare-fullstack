d5cfeb58169db22feeeceea9f7e62c5584ea90a7
jahir alam <work.jahiralam@gmail.com>
2024-01-09 22:59:11 +0600

    check

--- src/db/supabasePg.connect.ts
+++ src/db/supabasePg.connect.ts
@@ -2,6 +2,9 @@ import postgres from 'postgres';
 import { SERVER_ENV } from '../SERVER_ENV';
 import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { schemaSupabase } from './schemas/schemaSupabase';
+
+
+
 export const ConnectSupabasePg = (): PostgresJsDatabase<typeof schemaSupabase> => {
 	const client: postgres.Sql<any> = postgres(SERVER_ENV.SUPABASE_PG_URL);
 	return drizzle(client, { schema: schemaSupabase });
